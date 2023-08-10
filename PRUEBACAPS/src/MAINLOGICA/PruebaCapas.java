1ef6433aee6d22ce3ff284777d052533db29ccb6
Alex P.L <111670520+Aperealazo@users.noreply.github.com>
2023-08-10 13:08:04 -0300

    Add files via upload

---
+++ PRUEBACAPS/src/MAINLOGICA/PruebaCapas.java
@@ -0,0 +1,15 @@
+
+package MAINLOGICA;
+
+import IGU.Pantalla;
+
+public class PruebaCapas {
+    public static void main(String[] args) {
+        Pantalla pantallita = new Pantalla();
+        pantallita.setVisible(true);
+        pantallita.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
